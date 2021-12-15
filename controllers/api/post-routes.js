ac66b8b9ec523f484f41ce6d9f6531952b112778
kngurley <kngurley25@gmail.com>
2021-12-14 21:43:59 -0600

    add get post by id

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -31,4 +31,43 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            "id", 
+            "post_url",
+            "title",
+            "created_at"
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: "No post found that ID" });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
 module.exports = router;
