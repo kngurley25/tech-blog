d408ce5406ba4033f7a2a35626ef2a241bf96335
kngurley <kngurley25@gmail.com>
2021-12-14 22:04:05 -0600

    Post routes for post, put, delete

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -9,6 +9,7 @@ router.get("/", (req, res) => {
             "title",
             "created_at"
         ],
+        order: [["created_at", "DESC"]],
         include: [
             {
                 model: Comment,
@@ -67,7 +68,63 @@ router.get("/:id", (req, res) => {
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.body.user_id
     })
-})
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: "No post found with that ID" });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: "No post found with that ID" });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
