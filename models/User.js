31715aa3e4cb422455fd8eabbf36aa267c1c999d
kngurley <kngurley25@gmail.com>
2021-12-14 21:08:16 -0600

    add password hash

--- models/User.js
+++ models/User.js
@@ -1,33 +1,42 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-// TODO import bcrypt
+const bcrypt = require("bcrypt");
 class User extends Model {
     //TODO check password
 }
 
 User.init(
     {
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [8]
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
         }
-    }
     },
-    // TODO add hooks foo password hash
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         freezeTableName: true,
         underscored: true,
