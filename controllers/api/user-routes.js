5406104233d6fa989a943d5801b6faef5cbf6774
kngurley <kngurley25@gmail.com>
2021-12-14 21:19:50 -0600

    add login route with password validation

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -46,6 +46,26 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: "No username exists" });
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: "Incorrect password" });
+            return; 
+        }
+        res.json({ user: dbUserData, message: "You are now logged in" });
+    });
+});
+
 router.put("/:id", (req, res) => {
     User.update(req.body, {
         individualHooks: true,
--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
 class User extends Model {
-    //TODO check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
 }
 
 User.init(
