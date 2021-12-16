cf6a3090dc533cf12841ad00ce9001e69c7c15e0
kngurley <kngurley25@gmail.com>
2021-12-15 19:47:43 -0600

    set up template homepage

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require("express");
 // TODO update controllers for express session
 const controllers = require("./controllers");
 const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 // TODO add sequelize store
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
