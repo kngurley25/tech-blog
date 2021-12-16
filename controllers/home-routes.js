011917c22723b7acb8148970c00ca2274c86f7c1
kngurley <kngurley25@gmail.com>
2021-12-16 17:12:07 -0600

    add conditional rendering for login, comment form

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,7 +28,10 @@ router.get("/", (req, res) => {
     })
     .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render("homepage", { posts });
+        res.render("homepage", {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
     })
     .catch(err => {
         console.log(err);
@@ -80,7 +83,10 @@ router.get("/post/:id", (req, res) => {
             return;
         }
         const post = dbPostData.get({ plain: true });
-        res.render("single-post", { post });
+        res.render("single-post", {
+            post,
+            loggedIn: req.session.loggedIn
+         });
     })
     .catch(err => {
         console.log(err);
