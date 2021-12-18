c90d39f6a614097bf61abec06aa4e093767f1ace
kngurley <kngurley25@gmail.com>
2021-12-18 10:10:06 -0600

    update table for post text

--- server.js
+++ server.js
@@ -32,9 +32,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// TODO update this to require controllers
 app.use(controllers);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log("Now listening on " + PORT));
 })
